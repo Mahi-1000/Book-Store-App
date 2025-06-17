7141e8b1716b1c2f0d871cad5950289ee7d1697f
MAHI <114560242+Mahi-1000@users.noreply.github.com>
2025-06-17 10:51:40 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\src\\flutter_windows_3.29.2-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\flutter projects\\bookstoreapp" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\src\\flutter_windows_3.29.2-stable\\flutter"
+  "PROJECT_DIR=D:\\flutter projects\\bookstoreapp"
+  "FLUTTER_ROOT=D:\\src\\flutter_windows_3.29.2-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\flutter projects\\bookstoreapp\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\flutter projects\\bookstoreapp"
+  "FLUTTER_TARGET=D:\\flutter projects\\bookstoreapp\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\flutter projects\\bookstoreapp\\.dart_tool\\package_config.json"
+)
